a501c87f5e0c129fec34cdcf7f231a17d9b0e5dc
Bram Kuijper <bramkuijper@gmail.com>
2016-02-02 11:22:27 +0000

    script file changed so that I can vary rates of periodic change before and after the perturbation

--- src/generate_runs.py
+++ src/generate_runs.py
@@ -12,13 +12,14 @@ mu_g = 0.01
 sdmu_g = 0.02
 mu_m = [ 0.01, 0 ]
 sdmu_m = 0.02
-phi = [ 0, 0.5 ]
+phi = [ 0 ]
 m = " 0 0 0 0 "
 var_p  = 0.1
 
 # the rate of change in each environment
-rate1 = [0.5, math.pi - 0.5 ] #list(numpy.arange(0,2*math.pi+2*math.pi/25,2*math.pi/25))
-rate2 = [0.5, math.pi - 0.5 ] #list(numpy.arange(0,2*math.pi+2*math.pi/25,2*math.pi/25))
+rate1 = list(numpy.arange(0,math.pi+math.pi/10,math.pi/10))
+print(rate1)
+rate2 = [0.5 ] #list(numpy.arange(0,2*math.pi+2*math.pi/25,2*math.pi/25))
 
 # vary the size of change in the mean environment 
 int_size1 = list(numpy.arange(0,5,5.0/25))
@@ -27,6 +28,10 @@ int_size2 = list(numpy.arange(0,5,5.0/25))
 # vary the direction of change in the mean environment (pos or neg)
 direction1 = [ -1, 1 ]
 direction2 = [ -1, 1 ]
+
+# vary the rate of change following perturbation
+rate1ptb = [None] #list(arange(-math.pi, math.pi, 2*math.pi/25))
+rate2ptb = [None] #list(arange(-math.pi, math.pi, 2*math.pi/25))
 
 ctr = 0;
 
@@ -42,14 +47,23 @@ for int_size_i in int_size1:
                 for mu_mi in mu_m:
                     for rate1_i in rate1:
                         for rate2_i in rate2:
-                            if rate1_i > rate2_i:
-                                continue
+#                            if rate1_i > rate2_i:
+#                                continue
 
-                            for phi_i in phi:
-                                phiptb = phi_i
+                            for rate1ptb_i in rate1ptb:
+                                for rate2ptb_i in rate2ptb:
 
-                                ctr += 1
-                                print("echo " + str(ctr))
-                                print(exe + " " + str(c) + " "  + str(mu_g) + " " + str(sdmu_g) + " " + str(mu_mi) + " " + str(sdmu_m) + " " + str(phi_i) + " " \
-                                        + m + " " + str(var_p) + " " + str(rate1_i) + " " + str(rate2_i) + " " + str(rate1_i) + " " + str(rate2_i) + " " \
-                                        + str(phiptb) + " " + str(int1ptb) + " " + str(int2ptb) + " 0")
+                                    if rate1ptb_i is None:
+                                        rate1ptb_i = rate1_i
+                                    
+                                    if rate2ptb_i is None:
+                                        rate2ptb_i = rate2_i
+
+                                    for phi_i in phi:
+                                        phiptb = phi_i
+
+                                        ctr += 1
+                                        print("echo " + str(ctr))
+                                        print(exe + " " + str(c) + " "  + str(mu_g) + " " + str(sdmu_g) + " " + str(mu_mi) + " " + str(sdmu_m) + " " + str(phi_i) + " " \
+                                                + m + " " + str(var_p) + " " + str(rate1_i) + " " + str(rate2_i) + " " + str(rate1ptb_i) + " " + str(rate2ptb_i) + " " \
+                                                + str(phiptb) + " " + str(int1ptb) + " " + str(int2ptb) + " 0")
