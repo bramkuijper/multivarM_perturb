4d909aba82d33f1981ca2a6c3927b32782822d58
Bram Kuijper <bramkuijper@gmail.com>
2016-02-08 09:08:52 +0000

    prepared jobs generation file to vary amplitude

--- src/generate_runs_amplitude.py
+++ src/generate_runs_amplitude.py
@@ -23,6 +23,8 @@ var_p  = 0.1
 
 rate1 = [0, 0.5, math.pi - 0.5 ]
 rate2 = [0, 0.5, math.pi - 0.5 ] #list(numpy.arange(0,2*math.pi+2*math.pi/25,2*math.pi/25))
+
+ampl1 = [ 1.1, 1.25, 1.5, 1.75, 2.0 ]
 #
 # vary the size of change in the mean environment 
 int_size1 = [ 1 ] #list(numpy.arange(0,5,5.0/25))
@@ -33,8 +35,8 @@ direction1 = [ 1 ]
 direction2 = [ 1 ]
 #
 # vary the rate of change following perturbation
-rate1ptb = list(numpy.arange(-math.pi, math.pi, 2*math.pi/40))
-rate2ptb = list(numpy.arange(-math.pi, math.pi, 2*math.pi/40))
+rate1ptb = list(numpy.arange(-math.pi, math.pi, 2*math.pi/30))
+rate2ptb = list(numpy.arange(-math.pi, math.pi, 2*math.pi/30))
 
 ctr = 0;
 
@@ -47,26 +49,33 @@ for int_size_i in int_size1:
                 int1ptb = int_size_i * direction_i
                 int2ptb = int_size_j * direction_j
 
-                for mu_mi in mu_m:
-                    for rate1_i in rate1:
-                        for rate2_i in rate2:
-                            if rate1_i > rate2_i:
-                                continue
 
-                            for rate1ptb_i in rate1ptb:
-                                for rate2ptb_i in rate2ptb:
+                for ampl1_i in ampl1:
 
-                                    if rate1ptb_i is None:
-                                        rate1ptb_i = rate1_i
-                                    
-                                    if rate2ptb_i is None:
-                                        rate2ptb_i = rate2_i
+                    for mu_mi in mu_m:
+                        for rate1_i in rate1:
+                            for rate2_i in rate2:
+                                if rate1_i > rate2_i:
+                                    continue
 
-                                    for phi_i in phi:
-                                        phiptb = phi_i
+                                for rate1ptb_i in rate1ptb:
+                                    for rate2ptb_i in rate2ptb:
 
-                                        ctr += 1
-                                        print("echo " + str(ctr))
-                                        print(exe + " " + str(c) + " "  + str(mu_g) + " " + str(sdmu_g) + " " + str(mu_mi) + " " + str(sdmu_m) + " " + str(phi_i) + " " \
-                                                + m + " " + str(var_p) + " " + str(rate1_i) + " " + str(rate2_i) + " " + str(rate1ptb_i) + " " + str(rate2ptb_i) + " " \
-                                                + str(phiptb) + " " + str(int1ptb) + " " + str(int2ptb) + " 0")
+                                        if rate1ptb_i is None:
+                                            rate1ptb_i = rate1_i
+                                        
+                                        if rate2ptb_i is None:
+                                            rate2ptb_i = rate2_i
+
+                                        for phi_i in phi:
+                                            phiptb = phi_i
+
+                                            ctr += 1
+                                            print("echo " + str(ctr))
+                                            print(exe + " " + str(c) + " "  + str(mu_g) + " " + str(sdmu_g) 
+                                                    + " " + str(mu_mi) + " " + str(sdmu_m) + " " + str(phi_i) + " "
+                                                    + m + " " + str(var_p) + " " + str(rate1_i) + " " + str(rate2_i) + " " 
+                                                    + str(rate1ptb_i) + " " + str(rate2ptb_i) + " " \
+                                                    + str(phiptb) + " " + str(int1ptb) + " " + str(int2ptb)  
+                                                    + str(ampl1_i) + " " + str(ampl1_i) + " " + str(ampl1_i) + " " + str(ampl1_i) 
+                                                    + " 0")
